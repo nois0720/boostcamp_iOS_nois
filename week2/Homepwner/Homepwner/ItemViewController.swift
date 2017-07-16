267773ff0981b82e84889431f4a9fe5dda694749
nois0720 <dbtjr0720@gmail.com>
2017-07-16 17:13:00 +0900

    add Homepwner proj

---
+++ week2/Homepwner/Homepwner/ItemViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ItemViewController.swift
+//  Homepwner
+//
+//  Created by Yoo Seok Kim on 2017. 7. 16..
+//  Copyright © 2017년 Nois. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+}
